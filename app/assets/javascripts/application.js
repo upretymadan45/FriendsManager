43e7e91a6f4361c7ee6084b212fdb3b162186d09
Madan <upretymadan45@gmail.com>
2017-07-17 05:57:28 +1200

    bootstrap styling and toastr notification added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require rails-ujs
 //= require jquery/dist/jquery.js
 //= require bootstrap/dist/js/bootstrap.js
+//= require toastr/toastr.js
 //= require_tree .
--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,2 +1,55 @@
 class FriendsController < ApplicationController
+    before_action :authenticate_user!
+    def index
+        @friends = Friend.where('user_id=?',current_user.id)
+    end
+
+    def new
+        @friend = Friend.new
+    end
+
+    def create
+        @friend = Friend.new(friend_params)
+        @friend.user_id = current_user.id
+
+        if @friend.save
+            redirect_to friends_path, :notice => "New friend #{@friend.first_name} has been saved!"
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+        @user = User.find(current_user.id)
+        @friend = @user.friends.find(params[:id])
+    end
+
+    def update
+        @user = User.find(current_user.id)
+        @friend = @user.friends.find(params[:id])
+
+        if @friend.update_attributes(friend_params)
+            redirect_to friends_path, :notice => 'Your friend was updated successfully!'
+        else
+            render 'edit'
+        end
+    end
+
+    def show
+        @user = User.find(current_user.id)
+        @friend = @user.friends.find(params[:id])
+    end
+
+    def destroy
+        @friend = Friend.find(params[:id])
+        if @friend.destroy
+            redirect_to friends_path, :notice => 'Your friend was removed successfully'
+        end
+    end
+
+    private
+
+    def friend_params
+        params.require(:friend).permit(:first_name, :last_name, :user_id)
+    end
 end
